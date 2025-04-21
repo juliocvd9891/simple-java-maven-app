4c0af7c31fcb18814d556561665c2085db3f6a43
juliocvd9891 <julio.vargas.condor@gmail.com>
2025-04-21 15:05:23 -0600

    Update javamavenDSL.groovy

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,11 +1,12 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/juliocvd9891/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('juliocvd9891')
+            node / gitConfigEmail('julio.vargas.condor@gmail.com')
         }
     }
+	
     steps {
         maven {
           mavenInstallation('mavenjenkins')
